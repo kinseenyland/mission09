e19c1567b5f3443b2c256611a32c372b5b2a1904
kinseenyland <kinseenyland@gmail.com>
2024-03-08 16:24:24 -0700

    got the teams to show and imported to json file and added some style even though its not showing:(

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  tid: number;
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+const teams = teamsData.teams;
+
+function Welcome() {
+  return <h1>College Basketball Teams</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h2 className="card-title">{oneTeam.school}</h2>
+          <h3 className="card-subtitle mb-2 text-muted">
+            Mascot: {oneTeam.name}
+          </h3>
+          <h3 className="card-text">
+            Location: {oneTeam.city}, {oneTeam.state}
+          </h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        {teams.map((team) => (
+          // Wrap each team in a column div
+          <div key={`${team.tid}`} className="col-sm-12 col-md-6 col-lg-4 mb-4">
+            <Team {...team} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
